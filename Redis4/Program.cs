8049d3f1834eb0cd48c83fcfb133925c15eabcda
husney_bayona <husney_bayona@cnt.com.co>
2021-09-22 15:32:57 -0500

    Redis cache con C#

---
+++ Redis4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool _activo = true;
+
+            while (_activo)
+            {
+                Console.WriteLine("1 Guardar");
+                Console.WriteLine("2 Consultar");
+                Console.WriteLine();
+
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                if(value == 1)
+                {
+                    Guardar();
+                }else if( value == 2)
+                {
+                    Leer();
+                }
+
+                Console.WriteLine("¿Quiere continuar?");
+                Console.WriteLine("1 = si");
+                Console.WriteLine("2 = no");
+                Console.WriteLine();
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                if (option != 1)
+                    _activo = false;
+                
+            }  
+        }
+
+        public static void Guardar()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("¿Cual es la llave?");
+            string key = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("¿Cual es el valor?");
+            string value = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if(RedisCliDb.SET(key, value))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Guardado");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine();
+        }
+
+        public static void Leer()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("¿Cual es la llave del valor a consultar?");
+            string key = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string data = RedisCliDb.GET(key);
+
+            if(data != null)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("El valor es: ");
+                Console.WriteLine(data);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Error al consultar el valor");                
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Redis4/RedisCli.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+
+namespace Redis
+{
+    public static class RedisCliDb
+    {
+        
+        private static string _host = "localhost";
+        private static int _port = 6378;
+
+        public static string GET(string key)
+        {
+            using (RedisClient cli = new RedisClient(_host, _port))
+                return cli.Get<string>(key);
+        }
+
+        public static bool SET(string key, string value)
+        {
+            try
+            {
+                using (RedisClient cli = new RedisClient(_host, _port))
+                    cli.Set(key, value, TimeSpan.FromSeconds(10));
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+    }
+}
